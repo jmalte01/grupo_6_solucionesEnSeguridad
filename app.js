ec52d6a3198811e9465e2522ce81d5677e137567
lrodriguez86 <rodriguezluis.info@gmail.com>
2021-02-07 19:31:59 -0300

    Subida del esqueleto

---
+++ app.js
@@ -0,0 +1,34 @@
+const express = require ('express');
+const path = require ('path');
+
+const app = express ();
+
+const publicPath = path.resolve (__dirname, './public');
+
+app.use(express.static (publicPath));
+
+app.set('puerto', process.env.PORT || 3000);
+
+app.listen (app.get('puerto'), () => {
+    console.log ( 'Servidor activo puerto 3000')
+} );
+
+app.get('/',(req,res)=>{
+    res.sendFile (path.resolve (__dirname, './views/index.html'))
+});
+
+app.get('/login',(req,res)=>{
+    res.sendFile (path.resolve (__dirname, './views/login.html'))
+});
+
+app.get('/register',(req,res)=>{
+    res.sendFile (path.resolve (__dirname, './views/register.html'))
+});
+
+app.get('/productCart',(req,res)=>{
+    res.sendFile (path.resolve (__dirname, './views/productCart.html'))
+});
+
+app.get('/productDetail',(req,res)=>{
+    res.sendFile (path.resolve (__dirname, './views/productDetail.html'))
+});
