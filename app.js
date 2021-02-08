d32d3f6211a8c042bbb10a798665ab282e729983
jmalte <juan.altemir@gmail.com>
2021-02-08 18:41:36 -0300

    se agregan secciones de login, register y mensaje, modificaciones al router, y se agrega el footer

--- app.js
+++ app.js
@@ -1,34 +1,46 @@
-const express = require ('express');
-const path = require ('path');
+const express = require('express');
+const path = require('path');
 
-const app = express ();
+const app = express();
 
-const publicPath = path.resolve (__dirname, './public');
+const publicPath = path.resolve(__dirname, './public');
 
-app.use(express.static (publicPath));
+app.use(express.static(publicPath));
 
-app.set('puerto', process.env.PORT || 3000);
+app.set('puerto', process.env.PORT || "3000");
 
-app.listen (app.get('puerto'), () => {
-    console.log ( 'Servidor activo puerto 3000')
+app.listen(app.get('puerto'), () => {
+    console.log( 'Servidor activo puerto 3000');
 } );
 
 app.get('/',(req,res)=>{
-    res.sendFile (path.resolve (__dirname, './views/index.html'))
+    res.sendFile(path.resolve(__dirname, './views/index.html'));
 });
 
 app.get('/login',(req,res)=>{
-    res.sendFile (path.resolve (__dirname, './views/login.html'))
+    res.sendFile(path.resolve(__dirname, './views/login.html'));
 });
 
 app.get('/register',(req,res)=>{
-    res.sendFile (path.resolve (__dirname, './views/register.html'))
+    res.sendFile(path.resolve(__dirname, './views/register.html'));
 });
 
 app.get('/productCart',(req,res)=>{
-    res.sendFile (path.resolve (__dirname, './views/productCart.html'))
+    res.sendFile(path.resolve(__dirname, './views/productCart.html'));
 });
 
 app.get('/productDetail',(req,res)=>{
-    res.sendFile (path.resolve (__dirname, './views/productDetail.html'))
+    res.sendFile(path.resolve(__dirname, './views/productDetail.html'))
 });
+
+
+app.post('/',(req,res)=>{
+    res.sendFile(path.resolve(__dirname, './views/index.html'));
+});
+
+app.post('/registerMessage',(req,res)=>{
+    res.sendFile(path.resolve(__dirname, './views/registerMessage.html'));
+});
+
+
+// Agregar ruta a catálogo
